b3a48014ee625eee454b6b1b990638c0c2f013f0
Zylern <47510180+Zylern@users.noreply.github.com>
2021-12-11 07:24:22 +0530

    Add files via upload

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -4,7 +4,9 @@
 from pyrogram import filters, emoji
 from WebStreamer.bot import StreamBot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from datetime import datetime
+START_TIME = datetime.now()
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'Hi, Send me a file to get an instant stream link.')
+    await m.reply(f'**Hi, Send me a file to get an instant direct link.**\n**Uptime: {str(datetime.now() - START_TIME).split('.')[0]}**')
